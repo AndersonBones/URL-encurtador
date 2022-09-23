22003d1ad0c97b2720ffbaff7f6e925d70c8e441
Anderson <tornadotutoriais@gmail.com>
2022-09-22 20:16:35 -0400

    set check url

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,33 +1,32 @@
-const Url = require('../models/UrlModel')
-const UrlGenerator = require('../models/UrlGenerator')
+const Url = require('../models/linkzinModel')
+const UrlGenerator = require('../models/linkzin')
 
 const home = (req, res)=>{
     res.render('pages/home')
 }
 
 async function newUrl(req, res){
-    let url = req.body.url;
     let code = UrlGenerator.GenerateCode();
     let click = 0;
 
-    if(url){
+    let data = await new Url({
+        code,
+        url:req.body.url,
+        click
+    })
+
+    try {
+        let url = new URL(req.body.url)
+        let shortnerUrl = process.env.APP_URL + newUrl.code;
+        let newUlr = data.save();
         
-        try {
-    
-            let newUlr = await new Url({
-                code,
-                url,
-                click
-            }).save();
+        res.render('pages/result',{shortnerUrl, url, click, code})
+        
+      } catch(err) {
+        res.status(400).render('pages/invalidUrl')
+      }
 
-            let shortnerUrl = process.env.APP_URL + newUlr.code;
-            res.render('pages/result',{shortnerUrl, url, click, code})
 
-        } catch (error) {
-            console.log(error.message)
-        }
-
-    }
 
 }
 
@@ -35,65 +34,53 @@ async function newUrl(req, res){
 async function shortnerUrl(req, res, next){
     let code = req.params.code;
 
-    if(code){
-        try {
-            let doc = await Url.findOneAndUpdate({code}, {$inc:{click:1}})
+    try {
+        let doc = await Url.findOneAndUpdate({code}, {$inc:{click:1}})
 
-            if(doc){
-                res.redirect(doc.url);
-            }
-            else{
-                res.status(404).render('pages/404')
-            }
-            
-        } catch (error) {
+        if(doc){
+            res.redirect(doc.url);
+        }
+        else{
             res.status(404).render('pages/404')
         }
-    }else{
         
+    } catch (error) {
+        res.status(404).render('pages/404')
     }
 }
 
 async function getStats(req, res){
     let code = req.params.code;
 
-    if(code){
-        try {
-            let doc = await Url.findOne({code})
+    try {
+        let doc = await Url.findOne({code})
 
-            if(doc){
-                res.render('pages/stats', {click: doc.click})
-            }
-            else{
-                res.status(404).render('pages/404')
-            }
-            
-        } catch (error) {
-            res.status(404).render('pages/404', {error}) 
+        if(doc){
+            res.render('pages/stats', {click: doc.click})
         }
-    }else{
-       
+        else{
+            res.status(404).render('pages/404')
+        }
+        
+    } catch (error) {
+        res.status(404).render('pages/404', {error}) 
     }
 }
 
 async function getClicks(req, res){
     let code = req.body.shortnerUrl.slice(process.env.APP_URL.length);
 
-    if(code){
-        try {
-            let doc = await Url.findOne({code})
+    try {
+        let doc = await Url.findOne({code})
 
-            if(doc){
-                res.redirect(`/${code}/stats`)
-                            
-            }else{
-                res.status(404).render('pages/404')
-            }
-        } catch (error) {
-            res.status(404).render('pages/404', {error, body:req.body})
+        if(doc){
+            res.redirect(`/${code}/stats`)
+                        
+        }else{
+            res.status(404).render('pages/404')
         }
-    }else{
-        res.status(404).render('pages/404')
+    } catch (error) {
+        res.status(404).render('pages/404', {error, body:req.body})
     }
 
 }
